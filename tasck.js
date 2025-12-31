af3b7c4da146d4e9cde07cb371967936609e7edc
yassine <yassine@gmail.com>
2025-12-31 12:36:47 +0100

    ajoute if

--- tasck.js
+++ tasck.js
@@ -2,6 +2,11 @@ const tasks=[];
 function addTask(task) {
     tasks.push(task)
     console.log("task add :",task);
+    if(!task){
+        console.log("EMPTY task");
+        return;
+        
+    }
     
 }
 module.exports={tasks,addTask} 

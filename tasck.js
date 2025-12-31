3a6b319d76ac72a7fc7cd57071936d11bb02c26b
yassine <yassine@gmail.com>
2025-12-31 12:30:46 +0100

    ajouter console

--- tasck.js
+++ tasck.js
@@ -2,6 +2,8 @@ const tasks=[];
 function addTask(task) {
     tasks.push(task)
     console.log("task add :",task);
+    console.log("logging task..");
+    
     
 }
 module.exports={tasks,addTask} 
